31194881a97ec7230d57380bff6a307d12b72c97
Zack Campbell <zajrik@users.noreply.github.com>
2021-07-08 00:17:38 -0500

    chore: clean up _stringify()

--- lib/src/GuraParser.dart
+++ lib/src/GuraParser.dart
@@ -847,104 +847,102 @@ class _GuraParser extends _Parser
 		if (value == null)
 			return 'null';
 
-		switch (valueType)
+		if (valueType == 'String')
+			return '"$value"';
+
+		if (valueType == 'bool')
+			return value.toString();
+
+		if (valueType == 'num' || valueType == 'int' || valueType == 'double')
 		{
-			case 'String':
-				return '"$value"';
+			// Handle infinity
+			if (value == double.infinity)
+				return 'inf';
 
-			case 'bool':
-				return value.toString();
+			// Handle negative infinity
+			else if (value == double.negativeInfinity)
+				return '-inf';
 
-			case 'num':
-			case 'int':
-			case 'double':
-				// Handle infinity
-				if (value == double.infinity)
-					return 'inf';
+			// Handle NaN
+			else if (value == double.nan)
+				return 'nan';
 
-				// Handle negative infinity
-				else if (value == double.negativeInfinity)
-					return '-inf';
-
-				// Handle NaN
-				else if (value == double.nan)
-					return 'nan';
-
-				// Otherwise return normal number
-				return value.toString();
-
-			// Generic types are a bit more complex so defer to checking the actual
-			// type, as any given list or map type will inherit from List/Map if
-			// they are not those types directly (i.e. _InternalLinkedHashMap, etc.)
-			default:
-				if (value is Map)
-				{
-					String result = '';
-
-					for (final MapEntry<dynamic, dynamic> entry in value.entries)
-					{
-						result += '${entry.key}:';
-
-						// If the entry value is a Map, split the stringified value by
-						// newline and indent each line before adding it to the result
-						if (entry.value is Map)
-						{
-							result += '\n';
-
-							final String stringifiedValue = _stringify(entry.value).trimRight();
-
-							for (final String line in stringifiedValue.split('\n'))
-								result += ' ' * 4 + line + '\n';
-						}
-
-						// Otherwise add the stringified value
-						else
-						{
-							result += ' ${_stringify(entry.value)}\n';
-						}
-					}
-
-					return result;
-				}
-				else if (value is List)
-				{
-					final bool shouldMultiline = value.any((element) => element is Map);
-
-					if (!shouldMultiline)
-						return '[${value.map((e) => _stringify(e)).join(', ')}]';
-
-					String result = '[';
-
-					for (final ListEntry<dynamic> entry in value.entries)
-					{
-						final String stringifiedValue = _stringify(entry.value).trimRight();
-
-						result += '\n';
-
-						// If the stringified value contains multiple lines, indent all
-						// of them and add them all to the result
-						if (stringifiedValue.contains('\n'))
-							result += stringifiedValue
-								.split('\n')
-								.map((element) => ' ' * 4 + element)
-								.join('\n');
-
-						// Otherwise indent the value and add to result
-						else
-							result += ' ' * 4 + stringifiedValue;
-
-						// Add a comma if this entry is not the final entry in the list
-						if (entry.index < value.length - 1)
-							result += ',';
-					}
-
-					result += '\n]';
-
-					return result;
-				}
-
-				throw TypeError();
+			// Otherwise return normal number
+			return value.toString();
 		}
+
+		// Generic types are a bit more complex so defer to checking the actual
+		// type, as any given list or map type will inherit from List/Map if
+		// they are not those types directly (i.e. _InternalLinkedHashMap, etc.)
+
+		if (value is Map)
+		{
+			String result = '';
+
+			for (final MapEntry<dynamic, dynamic> entry in value.entries)
+			{
+				result += '${entry.key}:';
+
+				// If the entry value is a Map, split the stringified value by
+				// newline and indent each line before adding it to the result
+				if (entry.value is Map)
+				{
+					result += '\n';
+
+					final String stringifiedValue = _stringify(entry.value).trimRight();
+
+					for (final String line in stringifiedValue.split('\n'))
+						result += ' ' * 4 + line + '\n';
+				}
+
+				// Otherwise add the stringified value
+				else
+				{
+					result += ' ${_stringify(entry.value)}\n';
+				}
+			}
+
+			return result;
+		}
+
+		if (value is List)
+		{
+			final bool shouldMultiline = value.any((element) => element is Map);
+
+			if (!shouldMultiline)
+				return '[${value.map((e) => _stringify(e)).join(', ')}]';
+
+			String result = '[';
+
+			for (final ListEntry<dynamic> entry in value.entries)
+			{
+				final String stringifiedValue = _stringify(entry.value).trimRight();
+
+				result += '\n';
+
+				// If the stringified value contains multiple lines, indent all
+				// of them and add them all to the result
+				if (stringifiedValue.contains('\n'))
+					result += stringifiedValue
+						.split('\n')
+						.map((element) => ' ' * 4 + element)
+						.join('\n');
+
+				// Otherwise indent the value and add to result
+				else
+					result += ' ' * 4 + stringifiedValue;
+
+				// Add a comma if this entry is not the final entry in the list
+				if (entry.index < value.length - 1)
+					result += ',';
+			}
+
+			result += '\n]';
+
+			return result;
+		}
+
+		throw TypeError();
 	}
 
 	/// Stringifies the given `Map<String, dynamic>` [data] into a Gura-compatible
